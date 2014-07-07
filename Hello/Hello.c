edae08b8a2aaba5117d809790dd2c9197f92a9fa
Ronnie.P.Thomas <ronnietom@gmail.com>
2014-07-07 17:43:17 +0530

    Added module parameters to Hello World

--- Hello/Hello.c
+++ Hello/Hello.c
@@ -6,15 +6,30 @@
 #include <linux/module.h>
 MODULE_LICENSE("Dual BSD/GPL");
 
+/*
+ *	Adding module parameters
+ *	You need to pass all the parameters while loading the module
+ *	Else, you might get a "verification failed/tainting kernel"
+ *	error message
+ */
+static int howmany = 1;
+static char* whom = "world";
+module_param(howmany, int, S_IRUGO);
+module_param(whom, charp, S_IRUGO);
+
 static int hello_init(void)
-{
-	printk(KERN_ALERT "Hello, world\n");
+{ 
+	int i;
+	for(i=0;i<howmany;i++)
+	{
+		printk(KERN_ALERT "Hello, %s\n" ,whom);
+	}
 	return 0;
 }
 
 static void hello_exit(void)
 {
-	printk(KERN_ALERT "Goodbye, cruel world\n");
+	printk(KERN_ALERT "Goodbye, cruel %s\n" ,whom);
 }
 
 module_init(hello_init);
